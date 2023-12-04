d06bc67f6471799ded5a5f42e85ed181a10f2b1e
Tania Ali <taniafali@gmail.com>
2023-12-04 14:38:56 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+/* The following line can be included in your src/index.js or App.js file */
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
+import DisplayList from './components/renderList';
+function App() {
+  return (
+    <div className="App">
+      <h1>Grocery Bot</h1>
+      <DisplayList/>
+    </div>
+  );
+}
+
+export default App;
