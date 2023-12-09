84483be35d1c6b17202d8ca01f2a37d41ff6b0b6
Tania Ali <taniafali@gmail.com>
2023-12-09 15:27:08 -0800

    Fix a bug in appending form fields & allow deleting them.

--- src/components/addRecipeForm.jsx
+++ src/components/addRecipeForm.jsx
@@ -11,13 +11,14 @@ import { metricDryConversionFactors } from '../utilities/addDryIngredients';
 import { metricWetConversionFactors } from '../utilities/addWetIngredients';
 import { isPlural } from '../utilities/findPlural';
 import { numericQuantity } from 'numeric-quantity';
-
+import { IoMdClose } from "react-icons/io";
 
 const RecipeForm = ({ unitSystem, addRecipe }) => {
     const [show, setShow] = useState(false);
     const [isFavorite, setFavorite] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+    const [htmlForAddItem, setHTML] = useState([{ item: "" }]);
     const [errors, setErrors] = useState({});
 
     const handleAddToFavorite = () => {
@@ -28,70 +29,48 @@ const RecipeForm = ({ unitSystem, addRecipe }) => {
     }
     let customaryHTML = [];
     let customaryHTMLDry = Object.keys(dryConversionFactors).map((factor, i) => (<option key={factor + 'dry' + i} value={factor + ' dry'}>{factor}&nbsp; *dry</option>));
-    let customaryHTMLWet = Object.keys(wetConversionFactors).map((factor, j) => (<option key={factor + 'wet'+ j} value={factor + ' wet'}>{factor}&nbsp; *liquid</option>));
+    let customaryHTMLWet = Object.keys(wetConversionFactors).map((factor, j) => (<option key={factor + 'wet' + j} value={factor + ' wet'}>{factor}&nbsp; *liquid</option>));
     customaryHTML.push(customaryHTMLDry);
     customaryHTML.push(customaryHTMLWet);
     let metricHTML = [];
     const metricHTMLDry = Object.keys(metricDryConversionFactors).map((factor, i) => (<option key={factor + 'dry' + i} value={factor + ' dry'}>{factor}&nbsp; *dry</option>));
-    const metricHTMLWet = Object.keys(metricWetConversionFactors).map((factor,j) => (<option key={factor + 'wet'+ j} value={factor + ' wet'}>{factor}&nbsp; *liquid</option>));
+    const metricHTMLWet = Object.keys(metricWetConversionFactors).map((factor, j) => (<option key={factor + 'wet' + j} value={factor + ' wet'}>{factor}&nbsp; *liquid</option>));
     metricHTML.push(metricHTMLDry, metricHTMLWet);
     let optionsHTML = unitSystem === 'customary' ? customaryHTML : metricHTML;
-    const [htmlForAddItem, setHTML] = useState([]);
+
+
     const handleAddFormEl = () => {
-        let html = (
-        <div key = {htmlForAddItem.length + 1} className='d-flex mt-3'>
-            <Form.Control
-                type="text"
-                placeholder="Name"
-                // autoFocus
-                autoComplete='on'
-                name="ingredientName"
-                isInvalid={!!errors.itemName}
-                onBlur={handleItemName}
-
-
-            />
-            <Form.Control.Feedback type="invalid">{errors.itemName}</Form.Control.Feedback>
-            <Form.Control
-                type="text"
-                placeholder="Amount"
-                className='mx-1'
-                name="ingredientAmount"
-                isInvalid={!!errors.itemAmount}
-                onChange={handleItemAmount}
-            />
-            <Form.Control.Feedback type="invalid">{errors.itemAmount}</Form.Control.Feedback>
-            <Form.Select aria-label="select unit for ingredient" name="ingredientUnit">
-                <option key="none dry" value="none dry">Select Unit</option>
-                {optionsHTML}
-            </Form.Select>
-            
-        </div>
-        );
-        setHTML([...htmlForAddItem, html]);
+        // adds a form field for taking user input for adding an ingredient / item to the recipe ingredients.
+        setHTML([...htmlForAddItem, { item: "" }]);
     }
-    const handleItemName = (e)=> {
-       let name = e.target.value;
-       if (isPlural(name)) {
-        setErrors({...errors, itemName: 'Please enter ingredient name in singular form!'});
-       } else if (name === ''){
-        setErrors({...errors, itemName: 'Please enter ingredient name!'});
-       } else {
-         setErrors({...errors, itemName: null})
-       }
+    const handleRemoveFormEl = (index) => {
+        // removes a form field for taking user input for adding an ingredient / item to the recipe ingredients.
+        const htmlList = [...htmlForAddItem];
+        htmlList.splice(index, 1);
+        setHTML(htmlList);
     }
-    const handleItemAmount = (e)=> {
-        if (isNaN(numericQuantity(e.target.value ))){
-            setErrors({...errors,  itemAmount: 'Please enter a valid numeric amount!'});
+    const handleItemName = (e) => {
+        let name = e.target.value;
+        if (isPlural(name)) {
+            setErrors({ ...errors, itemName: 'Please enter ingredient name in singular form!' });
+        } else if (name === '') {
+            setErrors({ ...errors, itemName: 'Please enter ingredient name!' });
         } else {
-            setErrors({...errors, itemAmount:null});
+            setErrors({ ...errors, itemName: null })
         }
     }
-    const handleName = (e)=> {
-        if (!e.target.value){
-            setErrors({...errors,  name: 'Recipe name is required!'});
+    const handleItemAmount = (e) => {
+        if (isNaN(numericQuantity(e.target.value))) {
+            setErrors({ ...errors, itemAmount: 'Please enter a valid numeric amount!' });
         } else {
-            setErrors({...errors,  name: null});
+            setErrors({ ...errors, itemAmount: null });
+        }
+    }
+    const handleName = (e) => {
+        if (!e.target.value) {
+            setErrors({ ...errors, name: 'Recipe name is required!' });
+        } else {
+            setErrors({ ...errors, name: null });
         }
     }
     return (
@@ -112,10 +91,10 @@ const RecipeForm = ({ unitSystem, addRecipe }) => {
 
                 </Modal.Header>
                 <Modal.Body>
-                    <Form onSubmit={(e) => { 
+                    <Form onSubmit={(e) => {
                         addRecipe(e, isFavorite, errors);
                         handleClose();
-                        }} 
+                    }}
                         id="addRecipeForm"
                     >
                         <Form.Group className="mb-3" controlId="recipeForm.ControlInput1">
@@ -130,14 +109,15 @@ const RecipeForm = ({ unitSystem, addRecipe }) => {
                                 onBlur={handleName}
 
                             />
-                            <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
-                       </Form.Group>
+
+                        </Form.Group>
+                        <Form.Control.Feedback type="invalid">{errors.name}</Form.Control.Feedback>
                         <Form.Group
                             className="mb-3"
                             controlId="recipeForm.ControlTextarea1"
                         >
                             <Form.Label>Method</Form.Label>
-                            <Form.Control as="textarea"  name="method"/>
+                            <Form.Control as="textarea" name="method" />
                         </Form.Group>
                         <Form.Group className="mb-3" controlId="recipeForm.ControlInput2">
                             <Form.Label>Tag/ Tags</Form.Label>
@@ -150,32 +130,38 @@ const RecipeForm = ({ unitSystem, addRecipe }) => {
                         <Form.Group className="mb-3" controlId="recipeForm.ControlInput3">
                             <Form.Label>Ingredients</Form.Label>
                             <Button className='float-end' variant="secondary" onClick={handleAddFormEl}>ADD</Button>
+                            {htmlForAddItem.map((html, i) => (
+                                <div key={i} className='d-flex mt-3' name="ingredients">
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Name"
+                                        name="ingredientName"
+                                        isInvalid={!!errors.itemName}
+                                        onBlur={handleItemName}
+                                    />
+                                    <Form.Control.Feedback type="invalid">{errors.itemName}</Form.Control.Feedback>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder="Amount"
+                                        className='mx-1'
+                                        name="ingredientAmount"
+                                        isInvalid={!!errors.itemAmount}
+                                        onBlur={handleItemAmount}
+                                    />
+                                    <Form.Control.Feedback type="invalid">{errors.itemAmount}</Form.Control.Feedback>
+                                    <Form.Select aria-label="select unit for ingredient" name="ingredientUnit">
+                                        <option key="none dry" value="none dry">Select Unit</option>
+                                        {optionsHTML}
+                                    </Form.Select>
+                                    {htmlForAddItem.length > 1 &&
+                                        <div className="ms-2 border border-danger rounded" >
+                                            <IoMdClose size={36} onClick={() => handleRemoveFormEl(i)} />
+                                        </div>}
 
-                            <div key = {htmlForAddItem.length} className='d-flex mt-3' name="ingredients">
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Name"
-                                    name="ingredientName"
-                                    isInvalid={!!errors.itemName}
-                                    onBlur={handleItemName}
-                                />
-                                <Form.Control.Feedback type="invalid">{errors.itemName}</Form.Control.Feedback>
-                                <Form.Control
-                                    type="text"
-                                    placeholder="Amount"
-                                    className='mx-1'
-                                    name="ingredientAmount"
-                                    isInvalid={!!errors.itemAmount}
-                                    onChange={handleItemAmount}
-                                />
-                                  <Form.Control.Feedback type="invalid">{errors.itemAmount}</Form.Control.Feedback>
-                                <Form.Select aria-label="select unit for ingredient" name="ingredientUnit"/* autoFocus*/>
-                                    <option key="none dry" value="none dry">Select Unit</option>
-                                    {optionsHTML}
-                                </Form.Select>
 
-                            </div>
-                            {htmlForAddItem}
+                                </div>
+                            ))
+                            }
                         </Form.Group>
                     </Form>
                 </Modal.Body>
