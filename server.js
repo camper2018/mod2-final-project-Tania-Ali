35d9ea8f11a228422898c6eb7d04fb169f88a9d2
Tania Ali <taniafali@gmail.com>
2024-03-20 11:16:39 -0700

    Modify GET queries to eliminate the issue of duplicate ingredients

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT;
 const corsOptions = {
-  origin: '*', 
-  credentials: true,  
+  origin: '*',
+  credentials: true,
   'access-control-allow-credentials': true,
   optionSuccessStatus: 200,
 }
@@ -44,18 +44,18 @@ app.use(async (req, res, next) => {
 });
 app.use(express.json());
 // create database;
-app.get("/createdb",async (req, res) => {
+app.get("/createdb", async (req, res) => {
   const sql = "CREATE DATABASE IF NOT EXISTS recipedia";
   try {
     await req.db.query(sql);
     res.json("Database created");
-  } catch(err){
+  } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Error creating database'})
+    res.status(500).json({ message: 'Error creating database' })
   }
 });
 // Create tables
-app.get("/createTableIngredients",async (req, res) => {
+app.get("/createTableIngredients", async (req, res) => {
   const sql =
     `CREATE TABLE IF NOT EXISTS ingredients (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -70,9 +70,9 @@ app.get("/createTableIngredients",async (req, res) => {
   try {
     await req.db.query(sql);
     res.send("Ingredients table created");
-  } catch (err){
+  } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Error creating table "ingredients"'})
+    res.status(500).json({ message: 'Error creating table "ingredients"' })
   }
 
 });
@@ -87,9 +87,9 @@ app.get("/createTableTags", async (req, res) => {
   try {
     await req.db.query(sql);
     res.send("Tags table created");
-  } catch (err){
+  } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Error creating table "tags"'})
+    res.status(500).json({ message: 'Error creating table "tags"' })
   }
 
 });
@@ -105,17 +105,78 @@ app.get("/createTableRecipes", async (req, res) => {
   try {
     await req.db.query(sql);
     res.send("Recipes table created");
-  } catch (err){
+  } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Error creating table "recipes"'})
+    res.status(500).json({ message: 'Error creating table "recipes"' })
   }
 });
 // get random recipes of the requested count or limit from database
 
+// query using positional parameter
+// app.get('/recipes/:limit?', async function (req, res) {
+//   try {
+//     let limit = parseInt(req.params.limit) || 20;
+//     const sql = `
+//     SELECT
+//     r.id,
+//     r.name,
+//     CASE r.favorite
+//         WHEN 0 THEN false
+//         WHEN 1 THEN true
+//     END AS favorite,
+//     r.method,
+//     JSON_ARRAYAGG(
+//         JSON_OBJECT(
+//             'id', i.ingredient_id,
+//             'name', i.ingredient_name,
+//             'amount', i.amount,
+//             'unit', i.unit,
+//             'type', i.type,
+//             'category', i.category
+//         )
+//     ) AS ingredients,
+//     (
+//         SELECT JSON_ARRAYAGG(tag_name)
+//         FROM tags t
+//         WHERE t.recipe_id = r.id AND t.tag_name IS NOT NULL
+//     ) AS tags
+//   FROM
+//       recipes r
+//   LEFT JOIN
+//       (
+//           SELECT DISTINCT
+//               recipe_id,
+//               ingredient_id,
+//               ingredient_name,
+//               amount,
+//               unit,
+//               type,
+//               category
+//           FROM
+//               ingredients
+//       ) i ON r.id = i.recipe_id
+//   GROUP BY
+//       r.id, r.name, r.favorite, r.method
+//   ORDER BY
+//       RAND()
+//   LIMIT ?;
+// `
+//     const [result] = await req.db.query(sql, [ limit ]);
+//     console.log(result)
+//     res.status(200).json(result);
+
+//   } catch (err) {
+//     console.error('Error fetching recipes!', err);
+//     res.status(500).json({ message: 'Error fetching recipes' })
+//   }
+// });
+
+//query using named parameters
 app.get('/recipes/:limit?', async function (req, res) {
   try {
-    let limit = req.params.limit || '20';
-    const sql = `SELECT
+    let limit = parseInt(req.params.limit) || 20;
+    const sql = `
+    SELECT
     r.id,
     r.name,
     CASE r.favorite
@@ -123,18 +184,46 @@ app.get('/recipes/:limit?', async function (req, res) {
         WHEN 1 THEN true
     END AS favorite,
     r.method,
-    JSON_ARRAYAGG(JSON_OBJECT('id', i.ingredient_id, 'name', i.ingredient_name, 'amount', i.amount, 'unit', i.unit, 'type', i.type, 'category', i.category)) AS ingredients,
-    (SELECT JSON_ARRAYAGG(t.tag_name) FROM (SELECT DISTINCT tag_name FROM tags WHERE recipe_id = r.id AND tag_name IS NOT NULL)t) AS tags
-    FROM recipes r
-    LEFT JOIN ingredients i ON r.id = i.recipe_id
-    LEFT JOIN tags t ON r.id = t.recipe_id
-    GROUP BY r.id, r.name, r.favorite, r.method
-    ORDER BY RAND()
-    LIMIT ${limit};
-    `
-    const [result] = await req.db.query(sql);
+    JSON_ARRAYAGG(
+        JSON_OBJECT(
+            'id', i.ingredient_id,
+            'name', i.ingredient_name,
+            'amount', i.amount,
+            'unit', i.unit,
+            'type', i.type,
+            'category', i.category
+        )
+    ) AS ingredients,
+    (
+        SELECT JSON_ARRAYAGG(tag_name)
+        FROM tags t
+        WHERE t.recipe_id = r.id AND t.tag_name IS NOT NULL
+    ) AS tags
+  FROM
+      recipes r
+  LEFT JOIN
+      (
+          SELECT DISTINCT
+              recipe_id,
+              ingredient_id,
+              ingredient_name,
+              amount,
+              unit,
+              type,
+              category
+          FROM
+              ingredients
+      ) i ON r.id = i.recipe_id
+  GROUP BY
+      r.id, r.name, r.favorite, r.method
+  ORDER BY
+      RAND()
+  LIMIT :limit;
+`
+    const [result] = await req.db.query(sql, { limit });
+    console.log(result)
     res.status(200).json(result);
-    
+
   } catch (err) {
     console.error('Error fetching recipes!', err);
     res.status(500).json({ message: 'Error fetching recipes' })
@@ -144,26 +233,65 @@ app.get('/recipes/:limit?', async function (req, res) {
 // get a requested recipe based on search text from database
 // the text should either partially match recipe name or any of the recipe tag.
 
+// query using positional parameter
+
+// app.get('/search/:searchTerm', async function (req, res) {
+//   try {
+//     const searchTerm = (req.params.searchTerm).toLowerCase().trim();
+//     const sql = `
+//     SELECT
+//     r.id,
+//     r.name,
+//     r.favorite,
+//     r.method,
+//     JSON_ARRAYAGG(JSON_OBJECT('id', i.ingredient_id, 'name', i.ingredient_name, 'amount', i.amount, 'unit', i.unit, 'type', i.type, 'category', i.category)) AS ingredients,
+//     (SELECT JSON_ARRAYAGG(t.tag_name) FROM (SELECT DISTINCT tag_name FROM tags WHERE recipe_id = r.id AND tag_name IS NOT NULL)t) AS tags
+//     FROM recipes r
+//     LEFT JOIN ingredients i ON r.id = i.recipe_id
+//     LEFT JOIN tags t ON r.id = t.recipe_id
+// 	   WHERE r.name LIKE ? OR t.tag_name = ?
+//     GROUP BY r.id;
+//     `
+//     const searchText = `%${searchTerm}%`;
+
+//     const [result] = await req.db.query(sql, [searchText, searchTerm]);
+//     res.status(200).json(result);
+//   } catch (err) {
+//     console.error('Error searching recipes!', err);
+//     res.status(500).json({ message: 'Error searching recipes' })
+//   }
+// });
+
+// query using named parameter
 app.get('/search/:searchTerm', async function (req, res) {
   try {
     const searchTerm = (req.params.searchTerm).toLowerCase().trim();
+    const searchText = `%${searchTerm}%`;
     const sql = `
     SELECT
-    r.id,
-    r.name,
-    r.favorite,
-    r.method,
-    JSON_ARRAYAGG(JSON_OBJECT('id', i.ingredient_id, 'name', i.ingredient_name, 'amount', i.amount, 'unit', i.unit, 'type', i.type, 'category', i.category)) AS ingredients,
-    (SELECT JSON_ARRAYAGG(t.tag_name) FROM (SELECT DISTINCT tag_name FROM tags WHERE recipe_id = r.id AND tag_name IS NOT NULL)t) AS tags
-    FROM recipes r
-    LEFT JOIN ingredients i ON r.id = i.recipe_id
-    LEFT JOIN tags t ON r.id = t.recipe_id
-	  WHERE r.name LIKE ? OR t.tag_name = ?
-    GROUP BY r.id;
-    `
-    const searchText = `%${searchTerm}%`;
-
-    const [result] = await req.db.query(sql, [searchText, searchTerm]);
+        r.id,
+        r.name,
+        r.favorite,
+        r.method,
+        JSON_ARRAYAGG(JSON_OBJECT(
+            'id', i.ingredient_id,
+            'name', i.ingredient_name,
+            'amount', i.amount,
+            'unit', i.unit,
+            'type', i.type,
+            'category', i.category
+        )) AS ingredients,
+        (SELECT JSON_ARRAYAGG(tag_name) FROM (SELECT DISTINCT tag_name FROM tags WHERE recipe_id = r.id AND tag_name IS NOT NULL) t) AS tags
+    FROM
+        recipes r
+    LEFT JOIN
+        (SELECT DISTINCT recipe_id, ingredient_id, ingredient_name, amount, unit, type, category FROM ingredients) i ON r.id = i.recipe_id
+    WHERE
+        r.name LIKE :searchText OR r.id IN (SELECT DISTINCT recipe_id FROM tags WHERE tag_name LIKE :searchText)
+    GROUP BY
+        r.id, r.name, r.favorite, r.method;
+`;
+    const [result] = await req.db.query(sql, { searchText });
     res.status(200).json(result);
   } catch (err) {
     console.error('Error searching recipes!', err);
@@ -172,8 +300,37 @@ app.get('/search/:searchTerm', async function (req, res) {
 });
 
 // get recipe by id
+
+// query using positional parameter
+
+// app.get('/recipe/:id', async function (req, res) {
+//   const recipeId = req.params.id;
+//   const sql = `SELECT
+//     r.id,
+//     r.name,
+//     r.favorite,
+//     r.method,
+//     JSON_ARRAYAGG(JSON_OBJECT('id', i.ingredient_id, 'name', i.ingredient_name, 'amount', i.amount, 'unit', i.unit, 'type', i.type, 'category', i.category)) AS ingredients,
+//     (SELECT JSON_ARRAYAGG(t.tag_name) FROM (SELECT DISTINCT tag_name FROM tags WHERE recipe_id = r.id AND tag_name IS NOT NULL)t) AS tags
+//     FROM recipes r
+//     LEFT JOIN ingredients i ON r.id = i.recipe_id
+//     LEFT JOIN tags t ON r.id = t.recipe_id
+//     WHERE r.id = ?
+//     GROUP BY r.id, r.name, r.favorite, r.method
+//     ;`;
+
+//     const [result] = await req.db.query(sql, [recipeId]);
+//     if (result.length === 0) {
+//         res.status(404).json({ message: 'Recipe not found' });
+//     } else {
+//       res.json(result[0]);
+//     }
+// });
+
+// query using named parameter
 app.get('/recipe/:id', async function (req, res) {
   const recipeId = req.params.id;
+
   const sql = `SELECT
     r.id,
     r.name,
@@ -182,18 +339,18 @@ app.get('/recipe/:id', async function (req, res) {
     JSON_ARRAYAGG(JSON_OBJECT('id', i.ingredient_id, 'name', i.ingredient_name, 'amount', i.amount, 'unit', i.unit, 'type', i.type, 'category', i.category)) AS ingredients,
     (SELECT JSON_ARRAYAGG(t.tag_name) FROM (SELECT DISTINCT tag_name FROM tags WHERE recipe_id = r.id AND tag_name IS NOT NULL)t) AS tags
     FROM recipes r
-    LEFT JOIN ingredients i ON r.id = i.recipe_id
-    LEFT JOIN tags t ON r.id = t.recipe_id
-    WHERE r.id = ?
+    LEFT JOIN
+    (SELECT DISTINCT recipe_id, ingredient_id, ingredient_name, amount, unit, type, category FROM ingredients) i ON r.id = i.recipe_id
+    WHERE r.id = :recipeId
     GROUP BY r.id, r.name, r.favorite, r.method
     ;`;
 
-    const [result] = await req.db.query(sql, [recipeId]);
-    if (result.length === 0) {
-        res.status(404).json({ message: 'Recipe not found' });
-    } else {
-      res.json(result[0]);
-    }
+  const [result] = await req.db.query(sql, { recipeId });
+  if (result.length === 0) {
+    res.status(404).json({ message: 'Recipe not found' });
+  } else {
+    res.json(result[0]);
+  }
 });
 
 // save a recipe to database
@@ -237,33 +394,31 @@ app.get('/recipe/:id', async function (req, res) {
 app.post('/recipes', async function (req, res) {
   const recipe = req.body;
   const { name, favorite, method, ingredients, tags } = recipe;
-  const recipesQuery = "INSERT INTO recipes SET :recipe";
-  const ingredientsQuery = "INSERT INTO ingredients SET :ingredient";
-  const tagsQuery = "INSERT INTO tags SET :tag ";
-  console.log("I am inside POST")
+  const recipesQuery = "INSERT INTO recipes SET :recipe;";
+  const ingredientsQuery = "INSERT INTO ingredients SET :ingredient;";
+  const tagsQuery = "INSERT INTO tags SET :tag;";
   let recipeId;
   try {
     await req.db.beginTransaction();
     try {
-      const [result] = await req.db.query(recipesQuery, {recipe: { name, favorite, method }});
+      const [result] = await req.db.query(recipesQuery, { recipe: { name, favorite, method } });
       recipeId = result.insertId;
-      console.log("recipeId:", recipeId);
       await Promise.all(ingredients.map(async (ingredient) => {
         const { name, amount, unit, type, category } = ingredient;
-        await req.db.query(ingredientsQuery, {ingredient:{ recipe_id: recipeId, ingredient_name: name, amount: amount, unit: unit, type: type, category: category }});
+        await req.db.query(ingredientsQuery, { ingredient: { recipe_id: recipeId, ingredient_name: name, amount: amount, unit: unit, type: type, category: category } });
       }))
-      await Promise.all(tags.map(async (tag)=> {
-        await req.db.query(tagsQuery, {tag:{ recipe_id: recipeId, tag_name: tag?.trim() }});
+      await Promise.all(tags.map(async (tag) => {
+        await req.db.query(tagsQuery, { tag: { recipe_id: recipeId, tag_name: tag?.trim() } });
       }));
       await req.db.commit();
       res.status(201).json({ message: "Successfully added recipe with id = " + recipeId });
-    } catch(err){
+    } catch (err) {
       await req.db.rollback();
       res.status(500).json({ message: "Internal Server! Failed to save recipe with id = " + recipeId });
     }
-  } catch(err){
+  } catch (err) {
     console.log(recipeId);
-    res.status(500).json({ error: 'Database connection error' + err});
+    res.status(500).json({ error: 'Database connection error' + err });
   }
 });
 
@@ -309,31 +464,31 @@ app.post('/recipes', async function (req, res) {
 app.put('/recipes/:id', async (req, res) => {
   const recipeId = req.params.id;
   const { name, favorite, method, ingredients, tags } = req.body;
-  const updateRecipeQuery = `UPDATE recipes SET name = :name, favorite = :favorite, method = :method WHERE id = :id`;
+  const updateRecipeQuery = `UPDATE recipes SET name = :name, favorite = :favorite, method = :method WHERE id = :id;`;
   const deleteIngredientsQuery = `DELETE FROM ingredients WHERE recipe_id = :recipeId`;
-  const insertIngredientsQuery = `INSERT INTO ingredients (ingredient_name, amount, unit, type, category, recipe_id) VALUES :ingredientValues`;
-  const deleteTagsQuery = `DELETE FROM tags WHERE recipe_id=:recipeId`;
-  const insertTagsQuery = `INSERT INTO tags (tag_name, recipe_id) VALUES :tagValues`;
+  const insertIngredientsQuery = `INSERT INTO ingredients (ingredient_name, amount, unit, type, category, recipe_id) VALUES :ingredientValues;`;
+  const deleteTagsQuery = `DELETE FROM tags WHERE recipe_id=:recipeId;`;
+  const insertTagsQuery = `INSERT INTO tags (tag_name, recipe_id) VALUES :tagValues;`;
   try {
     await req.db.beginTransaction();
-    try { 
-      await req.db.query(updateRecipeQuery, {name, favorite, method, id: recipeId});
-      await req.db.query(deleteIngredientsQuery, {recipeId});
+    try {
+      await req.db.query(updateRecipeQuery, { name, favorite, method, id: recipeId });
+      await req.db.query(deleteIngredientsQuery, { recipeId });
       const ingredientValues = ingredients.map(ingredient => [ingredient.name, ingredient.amount, ingredient.unit, ingredient.type, ingredient.category, recipeId]);
-      await req.db.query(insertIngredientsQuery, {ingredientValues});
-      await req.db.query(deleteTagsQuery, {recipeId});
-      const tagValues = tags.map(tag => [tag , recipeId]);
-      await req.db.query(insertTagsQuery, {tagValues});
+      await req.db.query(insertIngredientsQuery, { ingredientValues });
+      await req.db.query(deleteTagsQuery, { recipeId });
+      const tagValues = tags.map(tag => [tag.trim(), recipeId]);
+      await req.db.query(insertTagsQuery, { tagValues });
       await req.db.commit();
       res.status(200).json({ message: 'Update successful' });
-    } catch(err){
-        // Rollback transaction if any update fails
-        await req.db.rollback();
-        // Send error response
-        console.error(err);
-        res.status(500).json({ error: 'Error updating recipe: '+ err });
+    } catch (err) {
+      // Rollback transaction if any update fails
+      await req.db.rollback();
+      // Send error response
+      console.error(err);
+      res.status(500).json({ error: 'Error updating recipe: ' + err });
     }
-  } catch(err){
+  } catch (err) {
     // Send error response if unable to start transaction or get connection
     res.status(500).json({ error: 'Database connection error' });
   }
@@ -363,16 +518,16 @@ app.put('/recipes/:id', async (req, res) => {
 app.delete('/recipes/:id', async (req, res) => {
   const recipeId = req.params.id;
   const deleteRecipeQuery = 'DELETE FROM recipes WHERE id = :recipeId';
-  try{
-    const [result] = await req.db.query(deleteRecipeQuery, {recipeId});
+  try {
+    const [result] = await req.db.query(deleteRecipeQuery, { recipeId });
     if (result.affectedRows === 0) {
       res.status(404).json({ error: 'Recipe not found!' });
     } else {
       res.status(200).json({ message: 'Recipe deleted successfully. ', recipeId })
     }
-  } catch(err){
+  } catch (err) {
     console.error('Error deleting recipe:', error);
-    res.status(500).json({error:'Internal Server Error'})
+    res.status(500).json({ error: 'Internal Server Error' })
   }
 });
 // Start the server
