1556b3e4a83f40a53eab98637d8c836a4c7ecbcd
Tania Ali <taniafali@gmail.com>
2024-06-26 23:21:07 -0700

    Change db url to connect to Railway database

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ const corsOptions = {
   optionSuccessStatus: 200,
 }
 
-// const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
-// const pool = mysql.createPool(urlDB);
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-});
+const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDATABASE}`
+const pool = mysql.createPool(urlDB);
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   waitForConnections: true,
+// });
 app.use(cors(corsOptions));
 app.use(cookieParser());
 //to parse JSON bodies in POST and PUT requests
